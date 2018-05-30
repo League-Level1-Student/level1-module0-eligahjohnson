b1dc9e88fdedb3a940b527ca2dbaaa9309bfb322
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-29 19:19:57 -0700

    PhotoQuiz

--- src/BodyPartQuiz.java
+++ src/BodyPartQuiz.java
@@ -31,9 +31,9 @@ public class BodyPartQuiz {
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
-
+        int eligah = 0 ;
 		// 2. Set the size of the window in the initializeGui() method below
-
+        window.setSize(500, 500);
 		for (int i = 0; i < 4; i++) {
 			
 			// 4. Ask the user who this person is and store their answer
--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -24,19 +24,23 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
 
 		// 2. create a variable of type "Component" that will hold your image
-
+           Component eligah ;
 		// 3. use the "createImage()" method below to initialize your Component
-
+           eligah = createImage ("https://www.flightclub.com/media/catalog/product/cache/1/image/1600x1140/9df78eab33525d08d6e5fb8d27136e95/2/0/201539_1.jpg")   ;
 		// 4. add the image to the quiz window
-
+              quizWindow.add(eligah);
 		// 5. call the pack() method on the quiz window
-
+              quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+      String  frog = JOptionPane.showInputDialog("Do you Now the Name of Kanye West Shoes ? ");  
 		// 7. print "CORRECT" if the user gave the right answer
-
+     if(frog.equals("Yeezys")) {
+     System.out.println("your correct");
+     }
 		// 8. print "INCORRECT" if the answer is wrong
-
+     else {
+    	 System.out.println("incorrect");
+     }
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
 
 		// 10. find another image and create it (might take more than one line of code)
@@ -47,10 +51,10 @@ public class PhotoQuiz {
 
 		// 13. ask another question
 
-		// 14+ check answer, say if correct or incorrect, etc.
+		// 14+ check answer, say if correct or incorrect, etc.ther 
 
 	}
-
+	
 	private static Component createImage(String imageUrl) throws MalformedURLException {
 		URL url = new URL(imageUrl);
 		Icon icon = new ImageIcon(url);
