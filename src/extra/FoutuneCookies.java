a44850902f3dd5fd69f66efaba55a0a04fa9600a
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-28 19:22:29 -0700

    Myfirstswinggui FourtuneCookies

---
+++ src/extra/FoutuneCookies.java
@@ -0,0 +1,12 @@
+package extra;
+
+public class FoutuneCookies {
+	public void showButton() {
+	     System.out.println("Button clicked");
+	}
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
