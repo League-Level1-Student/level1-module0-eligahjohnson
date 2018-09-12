7825b0630850bee40530fe4d6726c8c6a799cec0
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-11 19:24:06 -0700

    Fortune Cookies

--- src/extra/FortuneCookies.java
+++ src/extra/FortuneCookies.java
@@ -14,7 +14,7 @@ public class FortuneCookies implements ActionListener {
 	     JFrame cat = new JFrame ();
 	     cat.setVisible(true);
 	     cat.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	     JButton taco = new JButton ();
+	     JButton taco = new JButton ("Click here for fortune !!!!");
 	     cat.add(taco);
 	     taco.addActionListener(this);
 	     int Doggie = new Random().nextInt(5);
@@ -32,11 +32,34 @@ public class FortuneCookies implements ActionListener {
 	}
 	public static void main(String[] args) {
 	FortuneCookies eligah = new FortuneCookies ();
-	eligah.showButton();	
+	eligah.showButton();
+		  	     
 	}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		 JOptionPane.showMessageDialog(null, "WooHoo");
+		  int Doggie = new Random().nextInt(5);
+		     if(Doggie==0) {	     
+		     JOptionPane.showMessageDialog(null, "Your hair will turn pink");
+		     }
+		     else if (Doggie==1) {
+		    	 JOptionPane.showMessageDialog(null, "Your butt will despair ");
+		     }
+		     else if (Doggie==2) {
+		    	 JOptionPane.showMessageDialog(null," You will get on your next test");
+		    	 
+		     }
+		     else if (Doggie==3) {
+		    	 JOptionPane.showMessageDialog(null,"Your haur will fall off ");
+		    	 
+		     }
+		     else if (Doggie==4) {
+		    	 JOptionPane.showMessageDialog(null," I will be in your dreams");
+		    	 
+		     }
+		     else if (Doggie==5) {
+		    	 JOptionPane.showMessageDialog(null, "You will turn super sayin ");
+		     }
 		// TODO Auto-generated method stub
 		
 	}
